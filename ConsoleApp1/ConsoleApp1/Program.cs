fbaed54abc26667be4bf68557c819fcdf200f5b9
Pierre <Pierre@warpdevelopment.com>
2020-04-24 10:06:59 +0200

    Added git ignore

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = TimeSpan.FromSeconds(3662);
+
+            var test = $"{(int)time.TotalHours} Hour, {time.Minutes} minute, {time.Seconds} seconds";
+        }
+    }
+}
